7d35c0ec54de95627a169c6e2fb3fb38e1b52136
Ben Boeckel <ben.boeckel@kitware.com>
2014-02-08 00:49:09 -0500

    escape: Reserve space when escaping
    The string is going to be at least as long as the input, so reserve
    space ahead of time.

--- Source/cmLocalGenerator.cxx
+++ Source/cmLocalGenerator.cxx
@@ -3380,6 +3380,7 @@ cmLocalGenerator
 std::string cmLocalGenerator::EscapeForShellOldStyle(const std::string& str)
 {
   std::string result;
+  result.reserve(str.size());
 #if defined(_WIN32) && !defined(__CYGWIN__)
   // if there are spaces
   std::string temp = str;
@@ -3497,6 +3498,7 @@ std::string cmLocalGenerator::EscapeForCMake(const std::string& str)
 {
   // Always double-quote the argument to take care of most escapes.
   std::string result = "\"";
+  result.reserve(str.size());
   for(const char* c = str.c_str(); *c; ++c)
     {
     if(*c == '"')
